02b80b794ff1bc5971936fc30cb66d1e6bef32c6
Ivan Kornienko <ivkornienko2784@gmail.com>
2022-08-04 15:47:42 +0300

    add new activity

---
+++ app/src/main/java/com/demo/shoppinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.demo.shoppinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.shoppinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
